70b519be20832475fd72800c9617baeb25abbb28
pankil-soni <116267467+pankil-soni@users.noreply.github.com>
2024-07-23 19:36:23 +0530

    refactor: Update site name, logo and description in layout.tsx and site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "MediClarity",
+  description: "Understand Your Medical Documents with Ease.",
   navItems: [
     {
       label: "Home",
