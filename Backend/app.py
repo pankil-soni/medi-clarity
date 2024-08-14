83e4dc1933c6ff6199d0a76787125da6b4c9ed66
pankil-soni <116267467+pankil-soni@users.noreply.github.com>
2024-08-14 10:32:05 +0530

    add the backend app.py for getting response from OpenAI's GPT model

---
+++ Backend/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify, Response
+from PIL import Image
+import openai
+import base64
+import io
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins="*", methods=["GET", "POST"], allow_headers=["Content-Type"])
+openai.api_key = "REMOVED"
+
+initial_prompt = """
+
+##YUOUR TASK
+
+- Analyze the following given image of a doctor's prescription or medical report.
+- In response Please provide a clear and simple explanation of the medications prescribed that can be easily understood by a patient. 
+- Also provide a guide on what they should do next.
+- Do highlight any important information mentioned in the report.
+- Ensure that the explanation is general and harmless.
+- If medications are given in the given image then give a detailed explanation of each medication for eg. Dosage, Purpose, Frequency, Side effects, etc.
+- In the output firstly provide what is the report about then provide the important things that the patient should know about the report/prescription and then provide the explanation of the medications prescribed and lastly give a summary.
+"""
+
+
+def convert_image_to_base64(image_file):
+    with Image.open(image_file) as img:
+        rgb_img = img.convert("RGB")
+        buffered = io.BytesIO()
+        rgb_img.save(buffered, format="JPEG")
+        base64_encoded_image = base64.b64encode(buffered.getvalue())
+        base64_string = base64_encoded_image.decode()
+        return base64_string
+
+
+def get_gpt4_response(messages):
+
+    response = openai.chat.completions.create(
+        model="gpt-4o", temperature=0.7, messages=messages
+    )
+
+    return response.choices[0].message.content
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/analyze_prescription", methods=["POST"])
+def analyze_prescription():
+    if "image" not in request.files:
+        return jsonify({"error": "No image file provided"}), 400
+
+    image_file = request.files["image"]
+    if image_file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    base64_string = convert_image_to_base64(image_file)
+    base64url = f"data:image/jpeg;base64,{base64_string}"
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful Medical Assistant who is assisting a patient with understanding their doctor's prescription/reports.",
+        },
+        {"role": "user", "content": initial_prompt + base64url},
+    ]
+
+    gpt_response = get_gpt4_response(messages)
+
+    return Response(gpt_response, mimetype="text/plain")
+
+
+if __name__ == "__main__":
+    app.run()
