a2c804db968fd3796c13e89f36c7ea0a4b309cb3
pankil-soni <116267467+pankil-soni@users.noreply.github.com>
2024-08-14 11:40:08 +0530

    feat(backend): update initial prompt and image processing in prescription analysis route to Enhanced error handling and response generation for prescription analysis

--- Backend/app.py
+++ Backend/app.py
@@ -10,22 +10,23 @@ CORS(app, origins="*", methods=["GET", "POST"], allow_headers=["Content-Type"])
 openai.api_key = "REMOVED"
 
 initial_prompt = """
+## YOUR TASK
 
-##YUOUR TASK
-
-- Analyze the following given image of a doctor's prescription or medical report.
+- Analyze the following given image of a doctor's prescription or a medical report.
 - In response Please provide a clear and simple explanation of the medications prescribed that can be easily understood by a patient. 
 - Also provide a guide on what they should do next.
-- Do highlight any important information mentioned in the report.
+- Do highlight any important information mentioned in the report or prescription.
 - Ensure that the explanation is general and harmless.
 - If medications are given in the given image then give a detailed explanation of each medication for eg. Dosage, Purpose, Frequency, Side effects, etc.
 - In the output firstly provide what is the report about then provide the important things that the patient should know about the report/prescription and then provide the explanation of the medications prescribed and lastly give a summary.
+- If the given image is not a prescription or report then give a response that the given image is not a prescription or report.
+- Give detailed and clear response that can be easily understood by a patient.
 """
 
 
 def convert_image_to_base64(image_file):
     with Image.open(image_file) as img:
-        rgb_img = img.convert("RGB")
+        rgb_img = img.convert('RGB')
         buffered = io.BytesIO()
         rgb_img.save(buffered, format="JPEG")
         base64_encoded_image = base64.b64encode(buffered.getvalue())
@@ -36,41 +37,41 @@ def convert_image_to_base64(image_file):
 def get_gpt4_response(messages):
 
     response = openai.chat.completions.create(
-        model="gpt-4o", temperature=0.7, messages=messages
+        model="gpt-4o",
+        temperature=0.7,
+        messages=messages
     )
 
     return response.choices[0].message.content
 
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def hello():
     return "Hello, World!"
 
 
-@app.route("/analyze_prescription", methods=["POST"])
+@app.route('/analyze_prescription', methods=['POST'])
 def analyze_prescription():
-    if "image" not in request.files:
+    if 'image' not in request.files:
         return jsonify({"error": "No image file provided"}), 400
 
-    image_file = request.files["image"]
-    if image_file.filename == "":
+    image_file = request.files['image']
+    if image_file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
     base64_string = convert_image_to_base64(image_file)
     base64url = f"data:image/jpeg;base64,{base64_string}"
 
     messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful Medical Assistant who is assisting a patient with understanding their doctor's prescription/reports.",
-        },
-        {"role": "user", "content": initial_prompt + base64url},
+        {"role": "system", "content": "You are a helpful Medical Assistant who is assisting a patient with understanding their doctor's prescription/reports."},
+        {"role": "user",
+         "content": initial_prompt + base64url}
     ]
 
     gpt_response = get_gpt4_response(messages)
 
-    return Response(gpt_response, mimetype="text/plain")
+    return Response(gpt_response, mimetype='text/plain')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
